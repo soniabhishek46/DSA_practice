9072cf2d0f91bee74f28853fd9c9410bd05c8ef3
Abhishek Soni <113833503+soniabhishek46@users.noreply.github.com>
2024-08-25 14:38:33 +0530

    Initial commit 25-Aug-2024

--- python/count_digits_of_number.py
+++ python/count_digits_of_number.py
@@ -13,13 +13,3 @@ def count_no_of_digits(num: int):
 
 count_no_of_digits(123456)
 
-#-------------------------
-# Other way of doing this
-#-------------------------
-import math
-
-#Count number of digits in a number
-def count_num_of_digits(num: int):
-  return round(math.log(num, 10))
-
-print(count_num_of_digits(45345678901))
